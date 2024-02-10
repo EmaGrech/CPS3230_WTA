755d2aff46ae77c4a8865de2e5f11b0fe2aaa68d
EmaGrech <ema.grech.21@um.edu.mt>
2024-02-10 14:45:59 +0100

    Update CiderObject.java

--- src/test/java/CiderObject.java
+++ src/test/java/CiderObject.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -8,7 +9,7 @@ import java.time.Duration;
 
 public class CiderObject
 {
-    WebDriver driver;
+    static WebDriver driver;
 
     public CiderObject(WebDriver driver)
     {
@@ -42,12 +43,43 @@ public class CiderObject
     }
 
     //Actual Page Name for the shown Category Page
-    public String actualCategoryPageName()
+    public String actualPageName()
     {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement pageNameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sort-title-body")));
         String actualPageName = pageNameElement.getText();
 
         return actualPageName;
+    }
+
+    //Waits for product list to load
+    public static void productsLoaded()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product-list")));
+    }
+
+    public void usingTheSearchBar(String inputText)
+    {
+        //Accessing search bar
+        WebElement searchBar = driver.findElement(By.xpath("//div[@class='header-search']//input"));
+        searchBar.click();
+
+        //wait for searchbar to load
+        CiderObject.searchBarLoaded();
+
+        //Entering search request
+        WebElement searchInput = driver.findElement(By.className("search-model-input_inner"));
+        searchInput.sendKeys(inputText);
+        searchInput.sendKeys(Keys.ENTER);
+
+        //Make sure products are first loaded
+        CiderObject.productsLoaded();
+    }
+
+    public static void searchBarLoaded()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("search-model-input_inner")));
     }
 }
