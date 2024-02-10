ee03eb28fef59fef6ff51c45ea6f1dd27c5ff6c5
EmaGrech <ema.grech.21@um.edu.mt>
2024-02-10 14:46:02 +0100

    Update CiderTests.java

--- src/test/java/CiderTests.java
+++ src/test/java/CiderTests.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 
 public class CiderTests
@@ -62,14 +59,14 @@ public class CiderTests
         String expectedPageName = CiderObject.expectedCategoryPageName(category);
 
         //Get actual new page title
-        String actualPageName = CiderObject.actualCategoryPageName();
+        String actualPageName = CiderObject.actualPageName();
 
         Assertions.assertEquals(expectedPageName, actualPageName);
     }
 
     //To test if more than the minimum number products are being output
     @Test
-    public void minimumProductsAreMoreThan()
+    public void minimumProductsAreMoreThan_CategorySelection()
     {
         //Set minimum
         int minimum = 5;
@@ -78,8 +75,7 @@ public class CiderTests
         CiderObject.goToCategory("Sale");
 
         //Make sure products are first loaded
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product-list")));
+        CiderObject.productsLoaded();
 
         //Find all instances of products in the product list
         List<WebElement> products = driver.findElements(By.className("product-list-item-box"));
@@ -90,16 +86,65 @@ public class CiderTests
         Assertions.assertTrue(noOfProducts > minimum);
     }
 
-    /*
+    //To test if selecting the first product in the list takes it to its specific details page
     @Test
     public void accessSpecifiedProduct()
     {
+        //Going to specific Category
+        CiderObject.goToCategory("Sale");
 
+        //Make sure products are first loaded
+        CiderObject.productsLoaded();
+
+        //Locating first result
+        WebElement firstProduct = driver.findElement(By.className("product-item"));
+
+        //Get first result name
+        WebElement expectedFirstProduct = firstProduct.findElement(By.className("product-item-name"));
+        String expectedFirstProductName = expectedFirstProduct.getText();
+
+        //Accessing first result's details page
+        WebElement firstProductLink = firstProduct.findElement(By.className("cider-link"));
+        firstProductLink.click();
+
+        //Get name of output result details
+        WebElement actualFirstProduct = driver.findElement(By.className("product-detail-title"));
+        String actualFirstProductName = actualFirstProduct.getText();
+
+        Assertions.assertEquals(expectedFirstProductName, actualFirstProductName);
     }
 
+    //To test if using the search function works
     @Test
     public void successfulSearchResults()
     {
+        //using the search bar
+        String inputText = "shirt";
+        CiderObject.usingTheSearchBar(inputText);
 
-    } */
+        //getting the new page name
+        String actualPageName = CiderObject.actualPageName();
+
+        Assertions.assertEquals("Results for\n“" + inputText + "”", actualPageName);
+    }
+
+    //To test if search results are more than a specified minimum
+    @Test
+    public void minimumProductsAreMoreThan_SearchResult()
+    {
+        //Set minimum
+        int minimum = 5;
+
+        //using the search bar
+        String inputText = "shirt";
+        CiderObject.usingTheSearchBar(inputText);
+
+        //Find all instances of products in the product list
+        List<WebElement> products = driver.findElements(By.className("product-list-item-box"));
+
+        //number of products in list
+        int noOfProducts = products.size();
+
+        Assertions.assertTrue(noOfProducts > minimum);
+    }
 }
